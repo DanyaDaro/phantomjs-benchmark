5cb85484fa10ca1814bb4a836de82bd3173eea0e
Vitaliy Rudnyh <r@inbox.ru>
2015-08-14 21:10:36 +0500

    Main script

--- benchmark.js
+++ benchmark.js
@@ -8,22 +8,22 @@ var system = require('system'),
 
 function mean(array) {
   var sum = 0;
-  for(var k = 0; k < array.length; k++){
+  for(var k = 0; k < array.length; k++) {
     sum = parseFloat(sum) + parseFloat(array[k]);
   }
   return sum / array.length;
 }
 
-function benchmark(){
+function benchmark() {
   page.open(address, function () {
-    t = page.evaluate(function(){
+    t = page.evaluate(function() {
       return document.getElementsByTagName('timegenerate')[0].innerHTML;
     });
   });
   page.onLoadFinished = function() {
     array.push(t);
     console.log(count + ': Get value - ' + t);
-    if(count == iteration){
+    if(count == iteration) {
       console.log('Arithmetic mean: ' + mean(array));
       phantom.exit();
     }
